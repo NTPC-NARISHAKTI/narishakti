dcf2f6c35a7c7bc9b7e8125c910320b1f2efbe81
Adnan Faiz <adnanfaiz@Adnans-MacBook-Air.local>
2026-03-09 21:28:07 +0530

    adding protection ot end points

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,32 +3,37 @@ package routes
 import (
 	controllers "marketplace/internal/controller"
 
+	"marketplace/internal/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
+	auth := r.Group("/")
+	auth.Use(middleware.AuthMiddleware())
+	// Routes Private
 	// Project routes
 	r.POST("/projects", controllers.CreateProject)
-	r.GET("/projects", controllers.GetProjects)
-	r.GET("/projects/:id", controllers.GetProject)
-	r.PUT("/projects/:id", controllers.UpdateProject)
-	r.DELETE("/projects/:id", controllers.DeleteProject)
+	auth.GET("/projects", controllers.GetProjects)
+	auth.GET("/projects/:id", controllers.GetProject)
+	auth.PUT("/projects/:id", controllers.UpdateProject)
+	auth.DELETE("/projects/:id", controllers.DeleteProject)
 
 	// Product routes
-	r.POST("/products", controllers.CreateProduct)
-	r.GET("/products", controllers.GetProducts)
-	r.GET("/products/:id", controllers.GetProduct)
-	r.PUT("/products/:id", controllers.UpdateProduct)
-	r.DELETE("/products/:id", controllers.DeleteProduct)
+	auth.POST("/products", controllers.CreateProduct)
+	auth.GET("/products", controllers.GetProducts)
+	auth.GET("/products/:id", controllers.GetProduct)
+	auth.PUT("/products/:id", controllers.UpdateProduct)
+	auth.DELETE("/products/:id", controllers.DeleteProduct)
 
 	// User CRUD routes
-	r.POST("/users", controllers.CreateUser)
-	r.GET("/users", controllers.GetUsers)
-	r.GET("/users/:id", controllers.GetUser)
-	r.PUT("/users/:id", controllers.UpdateUser)
-	r.DELETE("/users/:id", controllers.DeleteUser)
+	auth.POST("/users", controllers.CreateUser)
+	auth.GET("/users", controllers.GetUsers)
+	auth.GET("/users/:id", controllers.GetUser)
+	auth.PUT("/users/:id", controllers.UpdateUser)
+	auth.DELETE("/users/:id", controllers.DeleteUser)
 
-	// Auth routes (register/login)
+	// Auth routes Public
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
 }
